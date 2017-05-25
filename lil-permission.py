a1a7a6380596d8edcaff43e256abc8ab962f4440
Rebecca Cremona <rcremona@law.harvard.edu>
2017-05-25 10:05:48 -0400

    Try revoking token when we are done with it.

--- lil-permission.py
+++ lil-permission.py
@@ -13,6 +13,7 @@ app.config['SECRET_KEY'] = environ.get('FLASK_SECRET_KEY')
 AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
 ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 ORGS_URL = 'https://api.github.com/user/orgs'
+REVOKE_TOKEN_URL = 'https://api.github.com/applications/{}/tokens/'.format(app.config['GITHUB_CLIENT_ID'])
 
 def login_required(func):
     @wraps(func)
@@ -56,6 +57,12 @@ def authorized():
         print 'Requesting User Organization Info'
         r = requests.get(ORGS_URL, headers={'accept': 'application/json',
                                             'authorization': 'token {}'.format(access_token)})
+
+        print 'Revoking Github Access Token'
+        # https://developer.github.com/v3/oauth_authorizations/#revoke-an-authorization-for-an-application
+        d = requests.delete(REVOKE_TOKEN_URL + access_token, auth=(app.config['GITHUB_CLIENT_ID'], app.config['GITHUB_CLIENT_SECRET']))
+        print '(Request returned {})'.format(d.status_code)
+
         data = r.json()
         if r.status_code == 200:
             if data and any(org['login'] == app.config['GITHUB_ORG_NAME'] for org in data):
